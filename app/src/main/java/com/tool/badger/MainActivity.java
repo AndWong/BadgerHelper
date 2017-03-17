8dd40936c3723de8cc4f2a3107518c2471033628
“AndWong” <990376527@qq.com>
2017-03-17 11:09:56 +0800

    temp

--- app/src/main/java/com/tool/badger/MainActivity.java
+++ app/src/main/java/com/tool/badger/MainActivity.java
@@ -13,11 +13,8 @@ public class MainActivity extends AppCompatActivity {
         new android.os.Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                int badgeCount = 1;
-//                ShortcutBadger.applyCount(MainActivity.this, badgeCount); //for 1.1.4+
-
                 try {
-                    BadgerUtil.createBadger(MainActivity.this,badgeCount);
+                    BadgerUtil.createBadger(MainActivity.this,1);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
